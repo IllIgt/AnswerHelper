3503ecbb6721f7b2a50486fa75d8f62e4df5c2c0
Alexey Ratnikov <Voblin_ne@mail.ru>
2018-03-25 04:10:14 +0300

    LongWayToOOPRefctoring

---
+++ Arrangement.py
@@ -0,0 +1,12 @@
+class Arrangement:
+    def __init__(self, location="", type_arr="",  exons="", omims="", kegg=""):
+        self.location = location
+        self.exons = exons
+        self.type_arr = type_arr
+        self.omims = omims
+        self.kegg=kegg
+
+
+
+
+
